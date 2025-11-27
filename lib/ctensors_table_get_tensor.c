d99c8d1833a2d5f2306f708466810a744b8a0a5f
Morgan Funtowicz <funtowiczmo@gmail.com>
2025-11-27 19:06:36 +0100

    misc: fix typo

--- lib/ctensors_table_get_tensor.c
+++ lib/ctensors_table_get_tensor.c
@@ -8,7 +8,7 @@
 #include <string.h>
 
 
-int find_tenor_by_name(const void* key, const void* tensor)
+int find_tensor_by_name(const void* key, const void* tensor)
 {
     const char* key_ = key;
     const ctensors_tensor_t* tensor_ = tensor;
@@ -21,7 +21,7 @@ ctensors_tensor_t* ctensors_table_get_tensor(const ctensors_table_t* table, cons
     if (table->num_tensors > 0)
     {
         const auto tensors = table->tensors;
-        return bsearch(name, tensors, table->num_tensors, sizeof(ctensors_tensor_t), find_tenor_by_name);
+        return bsearch(name, tensors, table->num_tensors, sizeof(ctensors_tensor_t), find_tensor_by_name);
     }
 
     return nullptr;
